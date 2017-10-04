af3b5dc8f1b57bc1ebc789272e174d6a4583112f
robomancer <elizabeth@therobomancer.com>
2017-10-04 09:32:31 -0600

    code annotation

--- visualizations.py
+++ visualizations.py
@@ -1,22 +1,26 @@
 #Elizabeth E. Esterly & Danny Byrd
 #visualizations.py
-#last updated 10/03/2017
+#last updated 10/04/2017
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 ####confusionMatrix########
+# takes a 2-d array, converts to a dataframe and prints to a text file
+# m ::: a 2-d array (matrix)
 def confusionMatrix(m):
 	df = pd.DataFrame(m)
 	np.savetxt('confusion.txt', df.values, fmt='%d')
 	return
 
-####betaIteration##########
+####betaAccuracyEvaluation##########
+# betaVal ::: adjust weight of prior and evaluate performance
 def betaAccuracyEvaluation(betaVal):
 	accuracy = None
 	return accuracy
 
 ###plotBetaAccuracies######
+# betas  :::  a list of beta values to be evaluated
 def plotBetaAccuracies(betas):
 	accuracies = []
 	for b in betas:
