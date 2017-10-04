e6002fb60e826abb56a159f72f43acaa191182c5
robomancer <elizabeth@therobomancer.com>
2017-10-04 09:45:21 -0600

    ideas for bayes structure

--- bayesForDays.py
+++ bayesForDays.py
@@ -2,3 +2,10 @@
 #bayesForDays.py
 #last updated 10/04/2017
 
+### read in the training data? or process online? Probably the latter this file is huge (1.5 G)
+
+### if we haven't seen this class before, it's a new one, make a new one
+
+###how many times have we seen this word and class combo
+
+
