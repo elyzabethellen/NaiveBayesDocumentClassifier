6a3c0d97bf2990374faf756e4f513deadaea2288
robomancer <elizabeth@therobomancer.com>
2017-10-04 09:49:49 -0600

    ideas for bayes structure

--- bayesForDays.py
+++ bayesForDays.py
@@ -4,7 +4,8 @@
 
 ### read in the training data? or process online? Probably the latter this file is huge (1.5 G)
 
-### if we haven't seen this class before, it's a new one, make a new one
+###create a matrix here: read in labels text file and index accordingly
+# maybe good to store a dictionary of label:index
 
 ###how many times have we seen this word and class combo
 
